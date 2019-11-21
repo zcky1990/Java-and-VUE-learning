6567490cfe6cf3147eb755711707e73ae685462d
Zaki Rizaldy <zcky1990@gmail.com>
2019-11-21 21:31:40 +0700

    add async service example

---
+++ backend-web/src/main/java/com/webapps/app/task/AsyncService.java
@@ -0,0 +1,28 @@
+package com.webapps.app.task;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import com.webapps.app.constants.Constant;
+import com.webapps.app.model.Users;
+import com.webapps.app.util.MailUtility;
+
+
+@Service
+public class AsyncService {
+	private static final Logger logger = LoggerFactory.getLogger(AsyncService.class);
+
+	@Async
+    public void sendSuccessSignUpMailService(Users user) throws InterruptedException {
+		logger.info("start sending mail");
+		MailUtility mUtil = new MailUtility();
+		String template = mUtil.getEmailTemplate(Constant.MAIL_TEMPLATE_PATH);
+		String messageBody = mUtil.getVerificationMailMessageBody(template, user);
+		mUtil.sendMail(messageBody, "", user.getEmail(), Constant.VERIFICATION_SIGN_UP_MAIL_SUBJECT);
+		logger.info("end sending mail");
+    }
+	
+}
