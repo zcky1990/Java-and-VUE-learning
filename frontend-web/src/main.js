a0d7a4e24ec23be4a8ac7c46d3fb9b9e1567cd0e
zaki@alodokter <soeltan.zaky@alodokter.com>
2019-11-22 15:35:24 +0700

    fix

--- frontend-web/src/main.js
+++ frontend-web/src/main.js
@@ -1,10 +1,17 @@
 import Vue from "vue";
-import VueSession from 'vue-session'
+import Vuetify from 'vuetify'
+import VueSession from "vue-session";
 import App from "./App.vue";
 import router from "./router";
+import  Util from "./util";
+import "vuetify/dist/vuetify.min.css"; 
+import CKEditor from "@ckeditor/ckeditor5-vue";
 
+Vue.use(Vuetify);
 Vue.config.productionTip = false;
-Vue.use(VueSession)
+Vue.use(VueSession);
+Vue.use(CKEditor);
+Vue.use(Util);
 
 new Vue({
   router,
