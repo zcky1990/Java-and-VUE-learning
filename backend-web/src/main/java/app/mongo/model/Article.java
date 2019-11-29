d726c1cf33e01434a6be8e0b41402c5d00b6575d
zaki@alodokter <soeltan.zaky@alodokter.com>
2019-11-29 13:15:41 +0700

    fix indexed

--- backend-web/src/main/java/app/mongo/model/Article.java
+++ backend-web/src/main/java/app/mongo/model/Article.java
@@ -7,6 +7,7 @@ import org.bson.types.ObjectId;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 
 import app.model.request.ArticleRequest;
